902cf7053231a21260deaf1de8cb8fd7e72c9f24
JorgenDIF <jorgenjonsson80@gmail.com>
2024-06-26 13:28:14 +0200

    feat: add FontAwesome, favicon, and adjust nav-links
    - Added FontAwesome for icons.
    - Added favicon to the project.
    - Pushed nav-links to the right using Bootstrap.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,19 @@ const NavBar = () => {
     return (
     <Navbar expand="lg" className="bg-body-tertiary">
   <Container>
-    <Navbar.Brand href="#home"> <img src={logo} alt="logo" height="45" /> </Navbar.Brand>
+    <Navbar.Brand> <img src={logo} alt="logo" height="45" /> </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+      <Nav className="ms-auto">
+        <Nav.Link>
+        <i className="fas fa-home"></i>Home
+        </Nav.Link>
+        <Nav.Link>
+        <i className="fas fa-sign-in-alt"></i>Sign in
+        </Nav.Link>
+        <Nav.Link>
+        <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
 
       </Nav>
     </Navbar.Collapse>
